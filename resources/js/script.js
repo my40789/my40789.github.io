f9117c84353d5cd23b5279c61dc461280bf0c1e2
Chuah Jun Sheng <chuahjunsheng@gmail.com>
2016-04-03 11:06:07 +0800

    add animation

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
 
 
+    /* For the sticky navigation */
     $('.js--section-features').waypoint(function (direction) {
         if (direction == 'down') {
             $('nav').addClass('sticky');
@@ -9,14 +10,36 @@ $(document).ready(function () {
         }
     }, {
         offset: '60px'
-    })
+    });
 
-    /*
-    var waypoints = $('#handler-first').waypoint(function(direction) {
-      notify(this.element.id + ' hit 25% from top of window') 
-    }, {
-      offset: '25%'
-    })
-    */
+
+    /* Scroll on button */
+    $('.js--scroll-to-plans').click(function () {
+        $('html, body').animate({
+            scrollTop: $('.js--section-plans').offset().top
+        }, 1000);
+    });
+
+    $('.js--scroll-to-start').click(function () {
+        $('html, body').animate({
+            scrollTop: $('.js--section-features').offset().top
+        }, 1000);
+    });
+
+
+    $(function () {
+        $('a[href*="#"]:not([href="#"])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
 
 });
